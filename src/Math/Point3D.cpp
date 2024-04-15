6f5e2dc2a73013f72287f026cde2b080951708be
Maniko <louis.chabrerie@epitech.eu>
2024-04-15 19:16:22 +0200

    Fix Point3D init

--- src/Math/Point3D.cpp
+++ src/Math/Point3D.cpp
@@ -6,3 +6,12 @@
 */
 
 #include "Point3D.hpp"
+
+raytracer::Point3D &raytracer::Point3D::operator=(const raytracer::Point3D &other)
+{
+    x = other.x;
+    y = other.y;
+    z = other.z;
+
+    return *this;
+}
--- src/Math/Point3D.hpp
+++ src/Math/Point3D.hpp
@@ -8,6 +8,7 @@
 #pragma once
 
 #include <utility>
+#include <iostream>
 #include "Vector3D.hpp"
 
 namespace raytracer
@@ -17,25 +18,16 @@ namespace raytracer
         public:
             double x, y, z;
 
-            Point3D(double x = 0, double y = 0, double z = 0) : x(0), y(0), z(0) {};
+            Point3D(double x = 0, double y = 0, double z = 0) : x(x), y(y), z(z) {};
             Point3D(const Point3D &other) : x(other.x), y(other.y), z(other.z) {};
-            Point3D(Point3D&& other) : x(std::move(other.x)), y(std::move(other.y)), z(std::move(other.z)) {};
+            Point3D(Point3D&& other) : x(other.x), y(other.y), z(other.z) {};
 
-            Point3D& operator=(const Point3D &other) {
-                if (this == &other) { return *this; }
-                x = other.x;
-                y = other.y;
-                z = other.z;
-            }
+            Point3D& operator=(const Point3D &other);
 
-            Point3D& operator=(Point3D&& other) {
-                if (this == &other) { return *this; }
-                x = std::move(other.x);
-                y = std::move(other.y);
-                z = std::move(other.z);
-            }
+            friend std::ostream& operator<<(std::ostream& os, const Point3D& p) { os << "Point3D(" << p.x << ", " << p.y << ", " << p.z << ")"; return os; }
 
             Point3D operator+(const Vector3D& vector) const { return Point3D(x + vector.x, y + vector.y, z + vector.z); }
             Vector3D operator-(const Point3D& other) const { return Vector3D(x - other.x, y - other.y, z - other.z); }
     };
+
 }
