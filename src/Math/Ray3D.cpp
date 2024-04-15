bad25bf2629d2d7e8a9732e2c515bdae86f5cde7
Maniko <louis.chabrerie@epitech.eu>
2024-04-15 19:16:35 +0200

    Fix Ray3D operators

--- src/Math/Ray3D.cpp
+++ src/Math/Ray3D.cpp
@@ -8,3 +8,11 @@
 #include "Ray3D.hpp"
 
 raytracer::Ray3D::~Ray3D() = default;
+
+raytracer::Ray3D &raytracer::Ray3D::operator=(const raytracer::Ray3D &other){
+    if (this == &other) { return *this; }
+    origin = other.origin;
+    direction = other.direction;
+
+    return *this;
+}
--- src/Math/Ray3D.hpp
+++ src/Math/Ray3D.hpp
@@ -19,19 +19,7 @@ namespace raytracer
             Ray3D(const Point3D &origin, const Vector3D &direction) : origin(origin), direction(direction) {};
             ~Ray3D();
 
-            Ray3D& operator=(Ray3D&& other) { // Move assignment operator
-                if (this == &other) { return *this; }
-                origin = std::move(other.origin);
-                direction = std::move(other.direction);
-                return *this;
-            }
-
-            Ray3D& operator=(const Ray3D& other) { // Copy assignment operator
-                if (this == &other) { return *this; }
-                origin = other.origin;
-                direction = other.direction;
-                return *this;
-            }
+            Ray3D& operator=(const Ray3D& other);
 
             Point3D origin;
             Vector3D direction;
