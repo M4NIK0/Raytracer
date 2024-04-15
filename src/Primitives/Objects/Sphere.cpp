fb6fe6772db07f461178e532a39c21ff566541ab
Maniko <louis.chabrerie@epitech.eu>
2024-04-15 19:20:33 +0200

    Add check if sphere is in front of ray and not behind

--- src/Primitives/Objects/Sphere.cpp
+++ src/Primitives/Objects/Sphere.cpp
@@ -17,10 +17,18 @@ bool raytracer::Sphere::hits(const raytracer::Ray3D &ray)
     double b = 2.0 * oc.dot(ray.direction);
     double c = oc.dot(oc) - radius * radius;
     double discriminant = b * b - 4 * a * c;
-    std::cout << "discriminant: " << discriminant << std::endl;
-    std::cout << "ray ori " << ray.origin << std::endl;
-    std::cout << "ray dir " << ray.direction << std::endl;
-    std::cout << "sphere ori " << position << std::endl;
 
-    return (discriminant >= 0);
+    if (discriminant < 0) {
+        return false;
+    } else {
+        double root1 = (-b - std::sqrt(discriminant)) / (2.0 * a);
+        double root2 = (-b + std::sqrt(discriminant)) / (2.0 * a);
+
+        // Check if the roots are positive
+        if (root1 > 0 || root2 > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
