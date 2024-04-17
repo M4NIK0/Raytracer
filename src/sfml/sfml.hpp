bc5f223498051fbeff96faa0948e53bf889336ad
Maniko <louis.chabrerie@epitech.eu>
2024-04-17 11:46:56 +0200

    Add display scale

--- src/sfml/sfml.hpp
+++ src/sfml/sfml.hpp
@@ -32,6 +32,16 @@ namespace raytracer
             Color operator*(double scalar) {
                 return Color(r * scalar, g * scalar, b * scalar);
             }
+
+            void cap() {
+                r = r > 255 ? 255 : r;
+                g = g > 255 ? 255 : g;
+                b = b > 255 ? 255 : b;
+
+                r = r < 0 ? 0 : r;
+                g = g < 0 ? 0 : g;
+                b = b < 0 ? 0 : b;
+            }
     };
 
     class IDisplay {
