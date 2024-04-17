7f91743b1d9e398234fd552b0751a05873b5c246
Maniko <louis.chabrerie@epitech.eu>
2024-04-17 11:46:25 +0200

    Add basic light implementation

---
+++ src/Light/Objects/PointLight.hpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2024
+** PointLight.hpp
+** File description:
+** raytracer
+*/
+
+#pragma once
+
+#include "../ILight.hpp"
+
+namespace raytracer
+{
+    class PointLight : public ILight
+    {
+        public:
+            PointLight(Color color, Point3D position, double intensity = 1) : _color(color), _position(position), _intensity(intensity) {};
+            ~PointLight();
+
+            Ray3D getLightRay(const Point3D &hitPoint) override;
+
+            Color getColor() override;
+
+            double getIntensity() override;
+
+            double getIntensityFromDistance(double distance) override;
+
+            Point3D getPosition() override { return _position; }
+
+            void setPosition(const Point3D &position) override { _position = position; }
+
+            void move(Vector3D vec) override { _position = _position + vec; }
+
+        private:
+            Color _color;
+            double _intensity;
+            Point3D _position;
+    };
+}
