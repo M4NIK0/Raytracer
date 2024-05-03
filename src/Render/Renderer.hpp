35d9c69b5ee1cbb66e0d6045a3acce776b80cd58
Maniko <louis.chabrerie@epitech.eu>
2024-05-03 11:13:41 +0200

    Separate RenderData class from Renderer

--- src/Render/Renderer.hpp
+++ src/Render/Renderer.hpp
@@ -13,35 +13,10 @@
 #include "../Light/ILight.hpp"
 #include "RenderRay.hpp"
 #include "RenderPoint.hpp"
+#include "RenderData.hpp"
 
 namespace raytracer
 {
-    class RenderData
-    {
-        public:
-            RenderData() = default;
-            ~RenderData() = default;
-
-            void initRenderBuffer();
-
-            std::vector<std::shared_ptr<IObject>> objects = {};
-            std::vector<std::shared_ptr<ILight>> lights = {};
-
-            size_t diffuseRays = 20;
-            size_t reflexionsRays = 5;
-            size_t maxBounces = 2;
-
-            std::vector<std::vector<Color>> renderBuffer = {};
-
-            int width = 1920;
-            int height = 1080;
-
-            int chunkWidth = 100;
-            int chunkHeight = 100;
-
-            int maxSamples = 5;
-    };
-
     class Renderer
     {
         public:
