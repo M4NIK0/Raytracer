0a02c513451db00bc6b760383c1c929e80c1a991
Maniko <louis.chabrerie@epitech.eu>
2024-05-08 21:13:34 +0200

    Remove multiple images gestion

--- src/main.cpp
+++ src/main.cpp
@@ -14,81 +14,76 @@
 #define CHUNK_SIZE_X 1
 #define CHUNK_SIZE_Y HEIGHT
 
-#define MAX_SAMPLES 35
+#define MAX_SAMPLES 150
 
 #include "Output/PPMOutput.hpp"
 
 int main()
 {
-    for (int i = 0; i < 100; i++)
-    {
-        std::cout << "Rendering frame " << i + 1 << " / 100" << std::endl;
+    raytracer::RenderProcessWrapper renderer(WIDTH, HEIGHT, 32);
+    renderer.renderer.camera.move(raytracer::Vector3D(0, 0, 2));
+    renderer.renderer.camera.sensitivity = 250;
+    renderer.renderer.camera.exposure = 0.1;
 
-        raytracer::RenderProcessWrapper renderer(WIDTH, HEIGHT, 8);
-        renderer.renderer.camera.move(raytracer::Vector3D(0, 0, 2));
-        renderer.renderer.camera.sensitivity = 250;
-        renderer.renderer.camera.exposure = 0.1;
+    renderer.renderer.renderData.chunkWidth = CHUNK_SIZE_X;
+    renderer.renderer.renderData.chunkHeight = CHUNK_SIZE_Y;
+    renderer.renderer.renderData.maxSamples = MAX_SAMPLES;
 
-        renderer.renderer.renderData.chunkWidth = CHUNK_SIZE_X;
-        renderer.renderer.renderData.chunkHeight = CHUNK_SIZE_Y;
-        renderer.renderer.renderData.maxSamples = MAX_SAMPLES;
-
-        auto obj1 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(0.5, -101, -4), 100,
-                                                        raytracer::Color(1, 1, 1));
-        auto obj2 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(/*-1.5*/0 + (double)i / 50, 0.5, -5), 1,
-                                                        raytracer::Color(1, 0, 1));
-        auto obj3 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(0.5, 1.7, -4), 0.1,
-                                                        raytracer::Color(1, 1, 1));
-        auto obj4 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(0.2, -0.3, -3), 0.7,
-                                                        raytracer::Color(1, 1, 1));
+    auto obj1 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(0.5, -101, -4), 100,
+                                                    raytracer::Color(1, 1, 1));
+    auto obj2 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(/*-1.5*/0, 0.5, -5), 1,
+                                                    raytracer::Color(1, 0, 1));
+    auto obj3 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(0.5, 1.7, -4), 0.1,
+                                                    raytracer::Color(1, 1, 1));
+    auto obj4 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(0.2, -0.3, -3), 0.7,
+                                                    raytracer::Color(1, 1, 1));
 //    auto obj5 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(0.8, 0, -109), 100, raytracer::Color(0, 1, 1));
-        auto obj5 = std::make_shared<raytracer::Plane>(raytracer::Point3D(0.8, 0, -109), 100,
-                                                       raytracer::Color(0, 1, 1));
-        auto obj6 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(3, 0, -4), 1, raytracer::Color(0, 0, 0));
+    auto obj5 = std::make_shared<raytracer::Plane>(raytracer::Point3D(0.8, 0, -109), 100,
+                                                   raytracer::Color(0, 1, 1));
+    auto obj6 = std::make_shared<raytracer::Sphere>(raytracer::Point3D(3, 0, -4), 1, raytracer::Color(0, 0, 0));
 
 
-        raytracer::Vector3D motion = raytracer::Vector3D(10, 0, 0);
-        raytracer::Vector3D rotation = raytracer::Vector3D(0, 0, 0);
+    raytracer::Vector3D motion = raytracer::Vector3D(10, 0, 0);
+    raytracer::Vector3D rotation = raytracer::Vector3D(0, 0, 0);
 
-        obj2->setMotion(motion, rotation);
-        obj6->setReflexionIndex(1);
+    obj2->setMotion(motion, rotation);
+    obj6->setReflexionIndex(1);
 
-        obj1->setReflexionIndex(0.5);
+    obj1->setReflexionIndex(0.5);
 
 
-        obj4->setGlassState(true);
+    obj4->setGlassState(true);
 
-        renderer.renderer.addObject(obj1);
-        renderer.renderer.addObject(obj2);
-        renderer.renderer.addObject(obj3);
-        renderer.renderer.addObject(obj4);
-        renderer.renderer.addObject(obj5);
-        renderer.renderer.addObject(obj6);
+    renderer.renderer.addObject(obj1);
+    renderer.renderer.addObject(obj2);
+    renderer.renderer.addObject(obj3);
+    renderer.renderer.addObject(obj4);
+    renderer.renderer.addObject(obj5);
+    renderer.renderer.addObject(obj6);
 
-        renderer.renderer.addLight(
-                std::make_shared<raytracer::PointLight>(raytracer::Color(255, 0, 0), raytracer::Point3D(-50, 200, -25),
-                                                        40000));
-        renderer.renderer.addLight(
-                std::make_shared<raytracer::PointLight>(raytracer::Color(0, 255, 0), raytracer::Point3D(0, 200, -25),
-                                                        40000));
-        renderer.renderer.addLight(
-                std::make_shared<raytracer::PointLight>(raytracer::Color(0, 0, 255), raytracer::Point3D(50, 200, -25),
-                                                        40000));
+    renderer.renderer.addLight(
+            std::make_shared<raytracer::PointLight>(raytracer::Color(255, 0, 0), raytracer::Point3D(-50, 200, -25),
+                                                    40000));
+    renderer.renderer.addLight(
+            std::make_shared<raytracer::PointLight>(raytracer::Color(0, 255, 0), raytracer::Point3D(0, 200, -25),
+                                                    40000));
+    renderer.renderer.addLight(
+            std::make_shared<raytracer::PointLight>(raytracer::Color(0, 0, 255), raytracer::Point3D(50, 200, -25),
+                                                    40000));
 
-        renderer.renderImageCLI();
+    renderer.renderImageDisplay();
 
-        // Create PPM Output
-        raytracer::PPMOutput output("./output" + std::to_string(i) + ".ppm", WIDTH, HEIGHT);
-        for (int y = 0; y < HEIGHT; y++)
+    // Create PPM Output
+    raytracer::PPMOutput output("./output.ppm", WIDTH, HEIGHT);
+    for (int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
         {
-            for (int x = 0; x < WIDTH; x++)
-            {
-                raytracer::Color color = renderer.renderer.renderData.renderBuffer[x][y];
-                output.setPixel(x, y, color);
-            }
+            raytracer::Color color = renderer.renderer.renderData.renderBuffer[x][y];
+            output.setPixel(x, y, color);
         }
-        output.writeToFile();
     }
+    output.writeToFile();
 
     return 0;
 }
