06f48ef01f581bb5906e032b31b8751b0e33a4a6
Arthur Bichara <arthur.bichara@epitech.eu>
2024-05-09 16:17:26 +0200

    Add render editor

--- src/Render/Editor/Editor.cpp
+++ src/Render/Editor/Editor.cpp
@@ -34,7 +34,32 @@ void raytracer::editor::run(int windowSize)
 
 void raytracer::editor::render()
 {
-    _display->clearWindow();
+    double maxColor = 0;
+
+    for (int i = 0; i < _editorData.width; i++)
+    {
+        for (int j = 0; j < _editorData.height; j++)
+        {
+            _imageBuffer[i][j] = _renderer.traceEditorRay(i, j);
+
+            if (_imageBuffer[i][j].color.r > maxColor)
+                maxColor = _imageBuffer[i][j].color.r;
+            if (_imageBuffer[i][j].color.g > maxColor)
+                maxColor = _imageBuffer[i][j].color.g;
+            if (_imageBuffer[i][j].color.b > maxColor)
+                maxColor = _imageBuffer[i][j].color.b;
+        }
+    }
+
+    for (int i = 0; i < _editorData.width; i++)
+    {
+        for (int j = 0; j < _editorData.height; j++)
+        {
+            _imageBuffer[i][j].color.r /= maxColor;
+            _imageBuffer[i][j].color.g /= maxColor;
+            _imageBuffer[i][j].color.b /= maxColor;
+        }
+    }
 }
 
 void raytracer::editor::handleEvents()
@@ -62,4 +87,18 @@ void raytracer::editor::handleMouseEvents()
 void raytracer::editor::handleMouseWheelEvents()
 {
 
+}
+
+void raytracer::editor::_initImage()
+{
+    _imageBuffer.clear();
+
+    for (int i = 0; i < _editorData.width; i++)
+    {
+        _imageBuffer.emplace_back();
+        for (int j = 0; j < _editorData.width; j++)
+        {
+            _imageBuffer[i].emplace_back();
+        }
+    }
 }
