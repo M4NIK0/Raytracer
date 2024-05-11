cf28ee06bdc5d7edf9f9f8bce2b47c9a61ef9eaa
Maniko <louis.chabrerie@epitech.eu>
2024-05-11 21:24:33 +0200

    Fix plane computation

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,21 @@
-#include <iostream>
 #include "Render/Camera.hpp"
 #include "Objects/Primitives/Sphere.hpp"
-#include "Objects/Primitives/Plane.hpp"
-#include "Objects/Primitives/Cube.hpp"
 #include "Objects/Advanced/WavefontObject.hpp"
-
 #include "Render/Editor/Editor.hpp"
 #include "sfml/sfml.hpp"
 #include "Render/RenderProcessWrapper.hpp"
-#include "Light/Objects/PointLight.hpp"
-
-#define WIDTH 640
-#define HEIGHT 360
-
-#define CHUNK_SIZE_X 16
-#define CHUNK_SIZE_Y 16
-
-#define MAX_SAMPLES 1
-
-#define MAX_THREADS 8
-
-#define MAX_BOUNCES 2
-
-#define MAX_DIFFUSE 2
-
-#define MAX_REFLEXIONS 2
-
-#define BASE_COMMAND_LINE false
-
-
-#include <chrono>
-#include "Render/Threads.hpp"
 #include "Output/PPMOutput.hpp"
 #include "Parser/Parser.hpp"
 #include "Parser/ParseArg.hpp"
 
 int main(int ac, char **av)
 {
-
     ParseArg parseArg(ac, av);
 
     std::string configFile = parseArg.getConfigFile();
     std::string outputFile = parseArg.getOutputFile();
-    int width = parseArg.getImageSize().first;
-    int height = parseArg.getImageSize().second;
+    int width = parseArg.getImageSize().second;
+    int height = parseArg.getImageSize().first;
     int chunkSizeX = parseArg.getChunkSize().first;
     int chunkSizeY = parseArg.getChunkSize().second;
     int windowSize = parseArg.getWindowSize();
